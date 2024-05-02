d55611433713b1ad7024fba8222a85b9619b3fbb
idletim3 <128943623+idletim3@users.noreply.github.com>
2024-05-02 16:35:34 +0200

    Create directory-fuzz.sh

---
+++ Attacking Web Applications with ffuf/directory-fuzz.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+read -p "Subdomains file path: " SUBDOMAINS
+read -p "URL: " URL
+read -p "Port: " PORT
+read -p "Recursion depth: " DEPTH
+
+while IFS= read -r line; do
+    ffuf -w "/usr/share/seclists/Discovery/Web-Content/directory-list-2.3-small.txt":FUZZ -u "http://$line.$URL:$PORT/FUZZ" -e ".php,.phps,.php7" -fl 1  -fc 403 -recursion -recursion-depth $DEPTH -t 64 -v -c
+done < "$SUBDOMAINS"
